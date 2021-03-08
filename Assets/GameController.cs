bd4078c1bc89a17606a992312694e1664d851db0
Danila Kuzmuk <Da.Kuzmuk@ssu.edu.ua>
2021-03-08 22:13:24 +0200

    Add files

---
+++ Assets/GameController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour
+{
+    [Header("Register Fields")]
+    [SerializeField]
+    private InputField RegLogin;
+    [SerializeField]
+    private InputField RegPassword;
+    [SerializeField]
+    private InputField RegRePassword;
+
+    [Header("Auth Fields")]
+    [SerializeField]
+    private InputField AuthLogin;
+    [SerializeField]
+    private InputField AuthPassword;
+
+    public GameObject MainMenuArray;
+    public GameObject RegArray;
+    public GameObject AuthArray;
+    public GameObject LobbyArray;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PlayerPrefs.DeleteAll();
+        if(PlayerPrefs.HasKey("login") && PlayerPrefs.HasKey("password"))
+        {
+
+            string log = PlayerPrefs.GetString("login");
+            string pass = PlayerPrefs.GetString("password");
+
+            AuthProcess(log, pass);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void Auth()
+    {
+
+        if (AuthLogin.text != string.Empty && AuthPassword.text != string.Empty )
+        {
+            AuthProcess(AuthLogin.text, AuthPassword.text);
+        }
+    }
+
+
+    public void AuthProcess(string login, string pass)
+    {
+        RankSystem.instanse.Auth(login, pass, (msg, status) =>
+        {
+            if (status)
+            {
+                PlayerPrefs.SetString("login", login);
+                PlayerPrefs.SetString("password", pass);
+                PlayerPrefs.Save();
+                AuthArray.SetActive(false);
+                MainMenuArray.SetActive(false);
+                LobbyArray.SetActive(true);
+            }
+            Debug.Log(msg + " " + status);
+
+        });
+    }
+
+    public void Register()
+    {
+        
+        if (RegLogin.text != string.Empty && RegPassword.text != string.Empty && RegPassword.text == RegRePassword.text)
+        {
+            RankSystem.instanse.Register(RegLogin.text, RegPassword.text, (msg, status) =>
+            {
+                                    Debug.Log(msg + " " + status);
+                if (status)
+                {
+
+                    PlayerPrefs.SetString("login", RegLogin.text);
+                    PlayerPrefs.SetString("password", RegPassword.text);
+                    PlayerPrefs.Save();
+                    RegArray.SetActive(false);
+                    LobbyArray.SetActive(true);
+                }
+            });
+        }
+    }
+
+
+    public void OpenAuth()
+    {
+        MainMenuArray.SetActive(false);
+        AuthArray.SetActive(true);
+    }
+
+    public void OpenReg()
+    {
+        MainMenuArray.SetActive(false);
+        RegArray.SetActive(true);
+    }
+
+    public void LogOut()
+    {
+
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/RankSystem/RankSystem.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
+
+public class RankSystem : MonoBehaviour
+{
+    public static RankSystem instanse = null; 
+
+    [Header("Connection settings")]
+    [SerializeField]
+    private string GameKey;
+    [SerializeField]
+    private string ServerAdress = "127.0.0.1:3000";
+
+
+    public delegate void Del(string message, bool result);
+
+
+    private string SessionKey = string.Empty;
+
+    private string TestConnectionPostHeade = "/testConnection";
+    private string RegisterPostHeade = "/registerClient";
+    private string AuthPostHeade = "/authClient";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instanse == null)
+        {
+            instanse = this;
+        }
+    }
+    
+    public void Connect(Del callback)
+    {
+        JObject jObject = new JObject();
+        if (GameKey != string.Empty && ServerAdress != string.Empty)
+        {
+            jObject["gameKey"] = GameKey;
+            StartCoroutine(PostRequest(ServerAdress + TestConnectionPostHeade, jObject.ToString(), (string result, bool status) => {
+                    callback(result, status);
+            }));
+        }
+
+    }
+
+    public void Register(string login, string password, Del callback)
+    {
+
+        JObject jObject = new JObject();
+        if (GameKey != string.Empty && ServerAdress != string.Empty)
+        {
+            jObject["gameKey"] = GameKey;
+            jObject["login"] = login;
+            jObject["password"] = password;
+            StartCoroutine(PostRequest(ServerAdress + RegisterPostHeade, jObject.ToString(), (string result, bool status) => {
+                JObject a = JObject.Parse(result);
+                if (a["error"] != null)
+                {
+                    callback(a["error"].ToString(), false);
+                }
+                else
+                {
+                    SessionKey = a["result"].ToString();
+                    callback(a["result"].ToString(), true);
+                }
+
+
+            }));
+        }
+    }
+
+    public void Auth(string login, string password, Del callback)
+    {
+        JObject jObject = new JObject();
+        if (GameKey != string.Empty && ServerAdress != string.Empty)
+        {
+            jObject["gameKey"] = GameKey;
+            jObject["login"] = login;
+            jObject["password"] = password;
+            StartCoroutine(PostRequest(ServerAdress + AuthPostHeade, jObject.ToString(), (string result, bool status) => {
+                JObject a = JObject.Parse(result);
+                if(a["error"] != null)
+                {
+                    callback(a["error"].ToString(), false);
+                }
+                else
+                {
+                    SessionKey = a["result"].ToString();
+                    callback(a["result"].ToString(), true);
+                }
+  
+            }));
+        }
+    }
+
+
+    public void TestConnection()
+    {
+        JObject jObject = new JObject();
+        if(GameKey != string.Empty && ServerAdress != string.Empty)
+        {
+            jObject["gameKey"] = GameKey;
+            StartCoroutine(PostRequest(ServerAdress + TestConnectionPostHeade, jObject.ToString(),(string result, bool state)=>{
+                Debug.Log("Received: " + result);
+            }));
+        }
+    }
+
+    IEnumerator PostRequest(string url, string json, Del callback)
+    {
+        var uwr = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+
+        //Send the request then wait here until it returns
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError)
+        {
+            callback(uwr.error, false);
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            callback(uwr.downloadHandler.text,true);
+
+        }
+    }
+
+
+    [CustomEditor(typeof(RankSystem))]
+    public class MyEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            RankSystem myScript = (RankSystem)target;
+            if (GUILayout.Button("Test Connection"))
+            {
+
+                myScript.TestConnection();
+            }
+
+        }
+    }
+}
